2bd9edcc5f3a242b048de9d49c4a4ff7aa9c904b
zhangcheng <zhangcheng@phillip.com.cn>
2022-03-02 17:26:57 +0800

    隐藏键盘

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -84,7 +84,10 @@ class _HomePageState extends State<HomePage> {
   Widget _checkableItem(
       bool isChecked, String title, Function(bool?) onCheckChanged) {
     return GestureDetector(
-      onTap: () => onCheckChanged(!isChecked),
+      onTap: () {
+        onCheckChanged(!isChecked);
+        _hideKeyboard();
+      },
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 4),
         decoration: BoxDecoration(
@@ -155,6 +158,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _startTouchFish() {
+    _hideKeyboard();
     try {
       int minutes = int.parse(_durationController.text);
       if (_fishOS == FishOS.macos) {
@@ -165,6 +169,13 @@ class _HomePageState extends State<HomePage> {
       setFishDuration(minutes);
     } catch (e) {
       print('start fish error: $e');
+    }
+  }
+
+  void _hideKeyboard() {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
+      FocusManager.instance.primaryFocus?.unfocus();
     }
   }
 }
